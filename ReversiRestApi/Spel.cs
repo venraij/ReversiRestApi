a000c01c1cc89ddb76c56aff6fdd2ff0fb96a617
Nick van Raaij <nick@yarle.nl>
2021-03-11 17:27:34 +0100

    Initial commit

---
+++ ReversiRestApi/Spel.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ReversiRestApi
+{
+    public class Spel : ISpel
+    {
+        public int ID { get; set; }
+        public string Omschrijving { get; set; }
+        public string Token { get; set; }
+        public string Speler1Token { get; set; }
+        public string Speler2Token { get; set; }
+        public Kleur[,] Bord { get; set; }
+        public Kleur AandeBeurt { get; set; }
+
+        public bool Afgelopen()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool DoeZet(int rijZet, int kolomZet)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Kleur OverwegendeKleur()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Pas()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool ZetMogelijk(int rijZet, int kolomZet)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
