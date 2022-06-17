7fbf283461c8f94f1bf1419a7d3467f1311fa15f
venraij <dagelijksnick@gmail.com>
2022-06-17 17:51:29 +0200

    fix urls

--- ReversiRestApiMVC/Startup.cs
+++ ReversiRestApiMVC/Startup.cs
@@ -31,18 +31,6 @@ namespace ReversiRestApiMVC
                 options.EnableDetailedErrors = true;
             });
             services.AddControllers().AddNewtonsoftJson();
-
-            services.AddCors(options =>
-            {
-                options.AddPolicy("MyPolicy",
-                                  builder =>
-                                  {
-                                      builder.WithOrigins("https://localhost:44303", "http://localhost:44303")
-                                      .AllowAnyHeader()
-                                      .AllowAnyMethod()
-                                      .AllowCredentials();
-                                  });
-            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -58,15 +46,14 @@ namespace ReversiRestApiMVC
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
+            
             app.UseHttpsRedirection();
             app.UseStaticFiles();
 
             app.UseRouting();
 
             app.UseAuthorization();
-
-            app.UseCors("MyPolicy");
-
+            
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
